dfffc36df767bbb9571fc2dfb5e7c6680e13d449
Brandon <blopez98@live.com>
2021-11-15 10:42:38 -0600

    Layout done

--- src/components/pages/index.vue
+++ src/components/pages/index.vue
@@ -1,11 +1,17 @@
 <template>
 <h1 class="text-yellow-800 text-5xl text-center bg-purple-200">Home</h1>
-<div class="container mx-auto text-yellow-300">
+<div class="container mx-auto text-gray-700">
 <div ><img class="px-80 py-3" src="https://media.istockphoto.com/photos/on-colourful-speech-bubbles-picture-id180819641?b=1&k=20&m=180819641&s=170667a&w=0&h=CX51cRVofQl95e_cu9Bfy5PLZQ1WdsqmJ-NCFzU96UI="></div>
 <div>
-    <h3 class="text-4xl">Welcome to the Online MarketPlace. The best place to purchase electronic devices.</h3>
+    <h3 class="text-4xl">Welcome to the Cooking Recipes site. You will be able to access the best recipes in the world., Just search any type of recipe you are looking for and we are sure to have it, good luck cooking!</h3>
 </div>
-<h3 class="text-4xl py-12">Feel free to navigate through our website so that you can see the items we offer and learn more about us.</h3>
-<img class="px-80" src="https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/9688850f-f59a-4a8c-af90-686efeadba94"/>
+<div class="py-10">
+    <input class="text-center w-full mt-14 rounded-full text-3xl" type="text" placeholder="Search for recipes here"/>
+</div>
+<div class="grid grid-cols-2 gap-5">
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti vero tempore nam sit, enim harum velit. Beatae quo quidem iste doloribus dolorem voluptatibus provident! Ducimus voluptate animi consequuntur pariatur laudantium!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione velit quasi sed, quibusdam veritatis voluptas? In error nulla adipisci eos, cum deleniti consectetur quos veniam autem, nesciunt, consequuntur fugit neque.</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, sunt nulla? Dolorum obcaecati incidunt ex commodi, dolores reprehenderit odit corporis illo quam accusamus modi corrupti et aspernatur aut earum nobis.</p>
+</div>
 </div>
 </template>
